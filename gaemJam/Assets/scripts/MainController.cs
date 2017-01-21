28d913d18fd9842c71e1b80e25449d7d7c6353aa
Mahtab Sabet <mahtabz@gmail.com>
2017-01-21 01:31:02 -0500

    Add skeleton functionality for generating either bullets or powerups

--- gaemJam/Assets/scripts/MainController.cs
+++ gaemJam/Assets/scripts/MainController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class MainController : MonoBehaviour {
 
+	public float powerupFrequency = 0.2f;
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +19,9 @@ public class MainController : MonoBehaviour {
 
     public void InstantiateGameObject(UnityEngine.Object to_clone)
     {
+		//Determine whether the projectile is a bullet or a powerup
+		//TODO: Determine if the to_clone object is tagged with "bullet" before calling "determineProjectileType()"
+		determineProjectileType();
 
         //for now, instantiate the object at 0, 0, 0. in future, check what kind of object it is and spawn appropriately accordingly.
         Instantiate(to_clone, new Vector3(0, 5, 0), Quaternion.identity);
@@ -29,4 +33,13 @@ public class MainController : MonoBehaviour {
         Destroy(to_terminate);
         print("i did it!\n");
     }
+		
+	private void determineProjectileType(){
+		float number = UnityEngine.Random.value;
+		if (number <= powerupFrequency) {
+			Debug.Log ("A powerup is generated! " + number);
+		} else {
+			Debug.Log ("A bullet is generated! " + number);
+		}
+	}
 }
