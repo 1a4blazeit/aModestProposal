bdf6151009b8e236958658704fab932d550c012f
Daniel Charpentier <d.y.charpen@gmail.com>
2017-01-20 20:24:26 -0500

    movement&Boolit
    added movement in the environment, a useless green rectangle and a
    bullet that flies up, and can be "fired" by pressing space

--- gaemJam/Assets/squareController.cs
+++ gaemJam/Assets/squareController.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#pragma strict
+
+
+
 public class squareController : MonoBehaviour {
+
+    public float speed;
+    public Vector3 gravity;
+    public Vector3 move;
 
 	// Use this for initialization
 	void Start () {
-		
+        speed = 0.5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+
+        move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed;
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
 }
